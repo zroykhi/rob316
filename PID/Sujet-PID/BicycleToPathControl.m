c3087378dbc2908563a0e7de02cfa2874a79f96b
zroykhi <727125137@qq.com>
2019-12-06 21:32:22 +0100

    error 355

--- PID/Sujet-PID/BicycleToPathControl.m
+++ PID/Sujet-PID/BicycleToPathControl.m
@@ -8,7 +8,7 @@ function [ u ] = BicycleToPathControl( xTrue, Path )
 % TODO
 %Q4
 Kp = 5;
-Ka = 15;
+Ka = 10;
 num_lines = length(Path) - 1;
 num_points = length(Path);
 step = 1;
@@ -27,10 +27,19 @@ for i=1:num_points
     end
 end
 
-if closest_point_idx == 3
-    turning_r = 0.6;
-    Ka = 50;
-    Kp = 4;
+if closest_point_idx == 2
+    turning_r = 0.3;
+    Ka = 5;
+    Kp = 10;
+elseif closest_point_idx == 3
+    turning_r = 0.5;
+    if min_dist_robot_2_vertice < 1
+        Ka = 90;
+        Kp = 50;
+    else
+        Ka = 20;
+        Kp = 15;
+    end
 elseif closest_point_idx == 4
     Ka = 15;
     Kp = 3;
@@ -39,27 +48,16 @@ elseif closest_point_idx == 5
     Ka = 18;
     Kp = 15;
     turning_r = 0.45;    
+elseif closest_point_idx == 6
+    Ka = 18;
+    Kp = 15;
+    turning_r = 0.45;       
 end    
 
 if closest_point_idx == 1
     current_phrase = 1;
 elseif closest_point_idx == 6
     current_phrase = 5;
-elseif closest_point_idx == 4
-    if min_dist_robot_2_vertice < turning_r
-        current_phrase = closest_point_idx;
-    else
-        error=abs(p_poly_dist(xTrue(1),xTrue(2),Path(1,:),Path(2,:)));
-        for i=2:4
-            % s = (x1y2-x1y3+x2y3-x2y1+x3y1-x3y2)
-            s = Path(1,i)*Path(2,i+1) - Path(1,i)*xTrue(2) + Path(1,i+1)*xTrue(2) - Path(1,i+1)*Path(2,i)+xTrue(1)*Path(2,i)-xTrue(1)*Path(2,i+1);
-            vector = Path(1:2,i+1) - Path(1:2,i);
-            d = abs(s) / sqrt(vector' * vector);
-            if abs(d - error) < 0.0001
-                current_phrase = i;
-            end
-        end
-    end
 else
     if min_dist_robot_2_vertice < turning_r
         current_phrase = closest_point_idx;
@@ -79,8 +77,6 @@ else
     end
 end
 
-error=p_poly_dist(xTrue(1),xTrue(2),Path(1,:),Path(2,:));
-
 start_point = Path(1:2,current_phrase);
 end_point = Path(1:2,current_phrase+1);
 vector = end_point - start_point;
@@ -91,7 +87,6 @@ dist_robot_to_goal = sqrt((xTrue(1:2,1) - xGoal)'*(xTrue(1:2,1) - xGoal));
 dist_robot_to_end = sqrt((xTrue(1:2,1) - end_point)'*(xTrue(1:2,1) - end_point));
 dist_goal_to_end = sqrt((xGoal - end_point)'*(xGoal - end_point));
 
-% while dist_robot_to_goal < dist_thredhold    
 while dist_goal_to_end>dist_robot_to_end | dist_robot_to_goal < dist_thredhold    
     xGoal = xGoal + step * unitVector';
     unitVector;
@@ -109,7 +104,7 @@ phi = Ka*apha;
 current_phrase;
 u = [v phi];
 
-if min_dist_robot_2_vertice < 0.05 && closest_point_idx == 6
+if min_dist_robot_2_vertice < 0.005 && closest_point_idx == 6
     u = [0 0];
 end    
 
