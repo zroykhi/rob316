68489f7a35179db911d887dad0f73e090e7248af
zroykhi <727125137@qq.com>
2019-12-08 11:38:42 +0100

    Q3 1627

--- PID/Sujet-PID/BicycleToPoseControl.m
+++ PID/Sujet-PID/BicycleToPoseControl.m
@@ -6,23 +6,14 @@ function [ u ] = BicycleToPoseControl( xTrue,xGoal )
 
 % TODO 
 %Q3
-Kp = 15;
-Ka = 15;
-Kb = 5;
+Kp = 60;
+Ka = 14; 
+Kb = -8; 
 dist = sqrt((xTrue(1) - xGoal(1))^2 + (xTrue(2) - xGoal(2))^2);
 apha = atan2(xGoal(2)-xTrue(2), xGoal(1)-xTrue(1)) - xTrue(3);
 beta = xGoal(3) - atan2(xGoal(2)-xTrue(2), xGoal(1)-xTrue(1));
 apha = AngleWrap(apha);
 beta = AngleWrap(beta);
-if dist < 0.1
-    Kp = 5;
-    Ka = 15;
-    Kb = 20;    
-else
-    Kp = 10;
-    Ka = 15;
-    Kb = 5;
-end  
 v = Kp*dist;
 phi = Ka*apha + Kb*beta; 
 
